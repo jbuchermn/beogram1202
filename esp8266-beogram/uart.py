1ef723b9a9c78eeea84f801b005152fcb80468a4
Jonas Bucher <j.bucher.mn@gmail.com>
2021-04-15 21:36:37 +0200

    Store csv

--- esp8266-beogram/uart.py
+++ esp8266-beogram/uart.py
@@ -14,6 +14,18 @@ class Runner(Thread):
         }
 
         self.pending_cmd = None
+        self.path_base = 'output_%d' % (int(time.time()) % 1000)
+
+    def save(self):
+        with open(self.path_base + '_delta_t.csv', 'w') as outp:
+            for t, v in self.data['delta_t']:
+                outp.write("%f,%f\n" % (t, v))
+        with open(self.path_base + '_avg_delta_t.csv', 'w') as outp:
+            for t, v in self.data['avg_delta_t']:
+                outp.write("%f,%f\n" % (t, v))
+        with open(self.path_base + '_control.csv', 'w') as outp:
+            for t, v in self.data['control']:
+                outp.write("%f,%f\n" % (t, v))
 
     def run(self):
         with serial.Serial('/dev/ttyUSB0', 74880) as ser:
@@ -22,16 +34,18 @@ class Runner(Thread):
                 if len(line) == 4:
                     if line[0] == ord('d'):
                         self.data['delta_t'] += [
-                            (time.time() - self.t0, (int(line[1]) * 256 + int(line[2])))
+                            (time.time() - self.t0, int(line[1]) * 256 + int(line[2]))
                         ]
                     elif line[0] == ord('a'):
                         self.data['avg_delta_t'] += [
-                            (time.time() - self.t0, (int(line[1]) * 256 + int(line[2])))
+                            (time.time() - self.t0, int(line[1]) * 256 + int(line[2]))
                         ]
                     elif line[0] == ord('f'):
                         self.data['control'] += [
-                            (time.time() - self.t0, (int(line[1]) * 256 + int(line[2]))/3)
+                            (time.time() - self.t0, int(line[1]) * 256 + int(line[2]))
                         ]
+
+                    self.save()
                 if self.pending_cmd is not None:
                     ser.write((self.pending_cmd + "\n").encode("ascii"))
                     self.pending_cmd = None
